887a864c36d7381a1e2b72c11ef54286db6876db
morosArtem <morosart1995@gmail.com>
2026-09-04 19:01:15 +0300

    Add files via upload

--- src/main.rb
+++ src/main.rb
@@ -1,27 +1,28 @@
 # src/main.rb
-# Точка входа: загружает данные, запускает роутинг, сохраняет результат.
 
 require 'json'
 require_relative 'routing'
 require_relative 'provider'
+require_relative 'soft_filter'
 
-# Пути к файлам (можно передать аргументами)
-PROVIDERS_PATH = 'data/providers.json'
-QUEUE_PATH = 'data/operations_queue_10.json'
-OUTPUT_PATH = 'routing_decisions.json'
+# ---- Вот это главное изменение ----
+# Строим пути относительно папки, где лежит main.rb
+BASE_DIR = __dir__   # это путь к папке src/
 
-# Загрузка данных
+PROVIDERS_PATH = File.join(BASE_DIR, '..', 'data', 'providers.json')
+QUEUE_PATH = File.join(BASE_DIR, '..', 'data', 'operations_queue_10.json')
+OUTPUT_PATH = File.join(BASE_DIR, '..', 'routing_decisions.json')
+# ---------------------------------
+
 def load_data
   providers_data = JSON.parse(File.read(PROVIDERS_PATH))
   queue = JSON.parse(File.read(QUEUE_PATH))
   [providers_data['providers'], queue]
 end
 
-# Точка входа
 if __FILE__ == $0
   providers_data, queue = load_data
   
-  # Можно задать свои веса для стратегии
   weights = {
     traffic: 0.20,
     volume: 0.20,
